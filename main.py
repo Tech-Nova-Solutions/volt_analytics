529ce81f376f7c5de49697f0c3c850662beed937
Leonardo Rocha Scarpitta <161969345+leonardorscarpitta@users.noreply.github.com>
2024-05-21 19:53:57 -0300

    Commit inicial
    Adicionando o arquivo do projeto

---
+++ main.py
@@ -0,0 +1,287 @@
+import os
+import plotly.graph_objects as go
+
+
+def clear():
+    print("\n" * 130)  # Comando para limpar o console
+    print(techNovaSolutions)  # Imprimir a logo da empresa e os divisores
+
+
+menuMsg = " Pressione qualquer tecla para retornar ao Menu."
+divisor = f"\n=====================↪↩=======================\n"
+techNovaSolutions = f"""
+{divisor} 
+ _____         _       _   _                  
+|_   _|__  ___| |__   | \ | | _____   ____ _  
+  | |/ _ \/ __| '_ \  |  \| |/ _ \ \ / / _` | 
+  | |  __/ (__| | | | | |\  | (_) \ V / (_| | 
+ _|_|\___|\___|_| |_|_|_|_\_|\___/ \_/ \__,_| 
+/ ___|  ___ | |_   _| |_(_) ___  _ __  ___    
+\___ \ / _ \| | | | | __| |/ _ \| '_ \/ __|   
+ ___) | (_) | | |_| | |_| | (_) | | | \__ \   
+|____/ \___/|_|\__,_|\__|_|\___/|_| |_|___/  
+{divisor}
+"""
+
+
+# FUNÇÃO PARA COMPRAR TICKETS PARA EVENTOS
+def comprarTickets(listaDeCompras):
+    adicionar = "s"
+    valorTotal = 0
+    while adicionar == "s":
+        print("[1] - Inteira: R$ 300,00")
+        print("[2] - Meia: R$ 150,00")
+        print("[3] - Cancelar operação")
+        print(divisor)
+        tipoDeIngresso = int(input("Selecione o tipo de Ingresso: "))
+        match tipoDeIngresso:
+            case 1:
+                numeroDeIngressos = int(input("Quantos ingressos você irá querer desse tipo? "))
+                valorTotal += 300 * numeroDeIngressos
+                for ingresso in range(numeroDeIngressos):
+                    listaDeCompras.append(f"🎫 x1 Ingresso Inteira")
+            case 2:
+                numeroDeIngressos = int(input("Quantos ingressos você irá querer desse tipo? "))
+                valorTotal += 150 * numeroDeIngressos
+                for ingresso in range(numeroDeIngressos):
+                    listaDeCompras.append(f"🎫 x1 Ingresso Meia")
+            case 3:
+                input(f"Operação cancelara! {menuMsg}")
+                main()
+            case _:
+                input(f"Opção inválida! {menuMsg}")
+                main()
+        adicionar = input("Deseja adicionar outro ingresso? [s/n]")
+
+    input(f"""✅ Ingressos comprados com sucesso! O valor total é de: R$ {valorTotal}!
+{menuMsg}""")
+    main()
+
+
+# FUNÇÃO PARA VERIFICAR OS ITENS NO CARRINHO E ADICIONAR NOVOS
+def verificarCarrinho(listaDeCompras, listaItensLoja):
+    if len(listaDeCompras) == 0:
+        print("Carrinho vazio")
+    else:
+        for i in range(len(listaDeCompras)):
+            print(f"{listaDeCompras[i]}")
+    clear()
+    print("[1] - Adicionar um novo item")
+    print("[2] - Limpar lista de compras")
+    print("[3] - Voltar ao menu")
+    opcao = int(input("Escolha uma opção - "))
+    match opcao:
+        case 1:
+            clear()
+            continuar = "s"
+            while continuar != "n":
+                print("Itens da Loja - Fórmula E")
+                print(divisor)
+                for item in listaItensLoja:
+                    print(item)
+                print(f"{divisor} ")
+                novoItem = input("Digite o nome do item: ")
+                if novoItem in listaItensLoja:
+                    novoItemFormatado = novoItem.capitalize()
+                    listaDeCompras.append(f"🚗 {novoItemFormatado}")
+                else:
+                    print('Erro! Item indisponível ou inexistente')
+                print(divisor)
+                for items in listaDeCompras:
+                    print(items)
+                print(divisor)
+                continuar = input("Deseja continuar? [s/n]")
+            verificarCarrinho(listaDeCompras, listaItensLoja)
+        case 2:
+            clear()
+            if len(listaDeCompras) > 0:
+                listaDeCompras.clear()
+                input(f"❌ Você removeu todos os itens do seu carrinho. {menuMsg}")
+            else:
+                input(f'Erro! Seu carrinho está vazio! {menuMsg}')
+            main()
+        case 3:
+            main()
+        case _:
+            input(f"Opção inválida! {menuMsg}")
+            main()
+
+
+def acessarDados():
+    print("[1] Comparação: Veículos da Fórmula 1 x Fórmula E x Fórmula Indy")
+    print("[2] Informações: Corredores brasileiros")
+    print(f"{divisor}")
+    opcao = int(input("Selecione uma opção- "))
+    match opcao:
+        case 1:
+            clear()
+            velocidadeVeiculos()
+        case 2:
+            clear()
+            acessarCorredores()
+        case _:
+            clear()
+            print(f"Opção inválida! {menuMsg}")
+
+
+def velocidadeVeiculos():
+    try:
+        import plotly.graph_objects as go
+
+        # Dados de exemplo
+        categorias = [
+            'Em repouso 0km/h',
+            '1.86s',
+            '3s',
+            'Velocidade Máxima (Registrada)'
+        ]
+
+        # Fórmula Indy
+        repouso_formula_indy = 0
+        um_oitentaedois_segundos_formula_indy = 100
+        velocidade_3_segundos_formula_indy = 162
+        velocidade_final_segundos_formula_indy = 380
+
+        # Fórmula E
+        repouso_formula_e = 0
+        um_oitentaedois_segundos_formula_e = 67
+        velocidade_3_segundos_formula_e = 110
+        velocidade_final_segundos_formula_e = 329
+
+        # Fórmula 1
+        repouso_formula_1 = 0
+        um_oitentaedois_segundos_formula_1 = 62
+        velocidade_3_segundos_formula_1 = 100
+        velocidade_final_segundos_formula_1 = 372
+
+        formula_e = [
+            repouso_formula_e,
+            um_oitentaedois_segundos_formula_e,
+            velocidade_3_segundos_formula_e,
+            velocidade_final_segundos_formula_e
+        ]
+
+        formula_1 = [
+            repouso_formula_1,
+            um_oitentaedois_segundos_formula_1,
+            velocidade_3_segundos_formula_1,
+            velocidade_final_segundos_formula_1
+        ]
+
+        formula_indy = [
+            repouso_formula_indy,
+            um_oitentaedois_segundos_formula_indy,
+            velocidade_3_segundos_formula_indy,
+            velocidade_final_segundos_formula_indy
+        ]
+
+        # Criando o gráfico de linhas comparativo
+        fig = go.Figure()
+
+        # Adicionando linha para Fórmula E
+        fig.add_trace(go.Scatter(
+            x=categorias, y=formula_e, mode='lines+markers',
+            name='Fórmula E', line=dict(color='blue')
+        ))
+
+        # Adicionando linha para Fórmula Indy
+        fig.add_trace(go.Scatter(
+            x=categorias, y=formula_indy, mode='lines+markers',
+            name='Fórmula Indy', line=dict(color='brown')
+        ))
+
+        # Adicionando linha para Fórmula 1
+        fig.add_trace(go.Scatter(
+            x=categorias, y=formula_1, mode='lines+markers',
+            name='Fórmula 1', line=dict(color='red')
+        ))
+
+        # Atualizando o layout do gráfico
+        fig.update_layout(
+            title='Velocidades que os Veículos da Fórmula 1 e Fórmula E atingem',
+            xaxis_title='Tempo',
+            yaxis_title='Velocidade',
+        )
+
+        # Exibindo o gráfico
+        fig.show()
+    except:
+        input(f"Erro! O programa não encontrou a biblioteca 'plotly'. {menuMsg}")
+
+
+# FUNÇÃO PARA ACESSAR A LISTA DE CORREDORES
+def acessarCorredores():
+    print(techNovaSolutions)
+
+    print("[1] - (ERT) Sérgio Sette Câmara")
+    print("[2] - (ABT) Lucas Di Grassi")
+    print(f"{divisor}")
+    corredorBiografia = int(input('Escolha um dos corredores para acessar sua biografia: '))
+
+    match corredorBiografia:
+        case 1:
+            input("""Sérgio Sette Câmara Filho, nascido em 23 de maio de 1998 em Belo Horizonte, 
+            é um piloto de automobilismo brasileiro. Ele competiu em várias categorias, incluindo Fórmula 3 
+            e Fórmula 2, antes de ingressar na Fórmula E.
+            Na Fórmula 3, Sérgio iniciou sua carreira em 2014, competindo no Brasil e no Europeu. Ele teve 
+            uma trajetória consistente, obtendo pódios e mostrando potencial. Sua participação destacada no 
+            Masters de Fórmula 3 e no Grande Prêmio de Macau chamou a atenção da Red Bull, que o contratou 
+            para seu programa de jovens pilotos.
+            Em 2017, Sérgio estreou na Fórmula 2, continuando a demonstrar habilidade e velocidade. Sua jornada 
+            na Fórmula 1 começou como piloto de testes da Toro Rosso e depois da McLaren. Ele retornou ao programa 
+            de jovens pilotos da Red Bull em 2020.
+            Em fevereiro de 2020, Sette Câmara fez a transição para a Fórmula E, inicialmente como reserva na Dragon 
+            Racing. Posteriormente, ele foi promovido a piloto titular e continuou a competir na categoria com a equipe 
+            NIO 333 Racing, que mais tarde foi rebatizada como ERT Formula E Team.""")
+        case 2:
+            input("""Lucas Di Grassi, nascido em São Paulo em 11 de agosto de 1984, é um piloto brasileiro 
+            de automobilismo, competindo atualmente na Fórmula E pela equipe ABT CUPRA. Sua carreira começou no kart 
+            em 1997, com rápidos sucessos, incluindo vitórias no sul-americano e pan-americano. Em 2004, entrou para o 
+            programa de desenvolvimento de jovens pilotos da equipe Renault de Fórmula 1, onde permaneceu por 4 anos.
+            Na GP2 Series, Di Grassi se destacou, conquistando um vice-campeonato em 2007 com a ART Grand Prix. Em 2010, 
+            estreou na Fórmula 1 com a equipe Virgin Racing. Após sua passagem na Fórmula 1, tornou-se piloto de testes da 
+            Pirelli em 2011.
+            Em 2014, Di Grassi entrou para a Fórmula E pela equipe Audi Sport ABT Formula E Team, conquistando o título na 
+            temporada 2016-17. Ao longo de sua carreira na Fórmula E, ele competiu por várias equipes, incluindo a ROKiT Venturi 
+            Racing e a Mahindra Racing, antes de se juntar à equipe ABT CUPRA Formula E Team em 2023.""")
+
+    main()
+
+
+# PROGRAMA - ESTRUTURA PRINCIPAL
+def main():
+    clear()
+    print("[1] - Comprar tickets")
+    print("[2] - Verificar carrinho")
+    print("[3] - Dados da Fórmula E")
+    print("[4] - Encerrar programa")
+    print(f"{divisor}")
+    opcao = int(input("Escolha uma opção - "))
+
+    match opcao:
+        case 1:
+            clear()
+            comprarTickets(listaDeCompras)
+        case 2:
+            clear()
+            verificarCarrinho(listaDeCompras, listaItensLoja)
+        case 3:
+            clear()
+            acessarDados()
+        case 5:
+            clear()
+            print("Encerrando o programa...")
+        case _:
+            clear()
+            print(f"Opção inválida! {menuMsg}")
+
+
+# Inicializar a lista de itens comprados pelo usuário
+listaDeCompras = []
+#Inicializar a lista de itens disponíveis na loja
+listaItensLoja = ["Bolsa - Fórmula E", "Camiseta - Fórmula E", "Calça - Fórmula E", "Blusa - Fórmula E"]
+
+# DEFAULT
+if __name__ == "__main__":
+    clear()
+    main()
